9abd030205955515e56379a474cd553bf0581cc7
Om Singh <omsingh06102005@gmail.com>
2025-02-14 23:08:00 +0530

    Done with both the frontend and backend part for registering and logging the user

--- api/index.js
+++ api/index.js
@@ -14,7 +14,10 @@ const app=express();
 const PORT=process.env.PORT
 console.log(PORT)
 app.use(express.json()); // This will parse the incoming request in json format
-app.use(cors());
+app.use(cors({
+    credentials:true,
+    origin:process.env.CORS_ORIGIN
+}));
 app.use(cookieParser());
 mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log("Database connected successfully"))
@@ -31,6 +34,9 @@ app.post('/register',async(req,res)=>{
             username:username,
             password:hashedPassword,
         })
+        .then(()=>{console.log("User created")})
+        .catch((err)=>{console.error("User already exists")});
+
         // In the jwt sign we are passing an empty object this used to contains the expiresIn property but we don't want to use it hence i am passing an empty object
         //Also in res.cookie() we are using secure:true it helps set the cookie on https request and is considered as best security practice.
         //Also the sameSite:"none" is used in cases suppose if our frontend is hosted on vercel and backend on render then if we don't use then only the backend would ne able to send cookies to the frontend else not.
@@ -49,6 +55,52 @@ app.post('/register',async(req,res)=>{
                 res.status(500).json({message:"Error creating user"});
             }
     }
+})
+app.get('/profile',(req,res)=>{
+    const token=req.cookies?.token;
+    if(token)
+        {
+            jwt.verify(token,jwtSecret,{},(err,userData)=>{
+                if(err) throw err
+                res.json(userData);
+            })
+            
+        }
+        else{
+            res.status(401).json('Invalid Token')
+        }
+});
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const foundUser = await User.findOne({ username });
+
+        // Check if user exists
+        if (!foundUser) {
+            return res.status(404).json({ message: "User doesn't exist" });
+        }
+
+        // Check if the password matches
+        const passOk = await bcrypt.compare(password, foundUser.password);
+        if (passOk) {
+            jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie("token", token, { sameSite: 'none', secure: true })
+                    .status(200)
+                    .json({ id: foundUser._id });
+            });
+        } else {
+            res.status(401).json({ message: "Invalid username or password" });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Error logging in" });
+    }
+});
+
+app.post('/logout',(req,res)=>{
+    res.cookie('token','',{sameSite:'none',secure:true}).json('ok');
 })
 app.listen(PORT,()=>{
     console.log(`Server is running on  http://localhost:${PORT}` );
---
+++ client/src/Routes.jsx
@@ -0,0 +1,11 @@
+import RegisterAndLoginForm from "./RegisterAndLoginForm"
+import { useContext } from "react"
+import { UserContext } from "./UserContext"
+
+const Routes = () => {
+  return (
+    <RegisterAndLoginForm />
+  )
+}
+
+export default Routes
