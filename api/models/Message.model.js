dd54794aa5b1ce8a2a02b2688050fcee2d03f971
Om Singh <omsingh06102005@gmail.com>
2025-02-17 11:15:29 +0530

    Added model inside file name and created a new schema to store the messages

---
+++ api/models/Message.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const messageSchema= new mongoose.Schema({
+    sender:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+    },
+    recipient:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+    },
+    text:{
+        type:String,
+    },
+    file:{
+        type:String,
+    }
+},{timestamps:true});
+export const Message=mongoose.model('Message',messageSchema);
